8e80e8060941df8b10906439bc546d0bfde04f22
rymbln <rymbln@gmail.com>
2014-06-17 17:11:39 +0400

    Revert "123"
    This reverts commit d698918ffb0b8f013f3bc85a6fbfdb723943561a.

---
+++ WPFDB/Data/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFDB.Model;
+
+namespace WPFDB.Data
+{
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> GetAllPersons();
+    }
+}
